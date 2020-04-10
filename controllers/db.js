b821aae14aa200487282cb6388c3ce115b1d380a
Robert Yorke <robert.yorke@gmail.com>
2020-04-10 13:01:33 +0000

    wee fix

--- controllers/db.js
+++ controllers/db.js
@@ -7,8 +7,8 @@ const mongoUrl = 'mongodb://localhost:27017';
 const dbName = 'coronavision';
 const collectionName = 'scores';
 
-const db = mongoClient.connect(url, config.mongoOptions)
-  .then((cli) => cli.db(dbName));
+const db = mongoClient.connect(mongoUrl, config.mongoOptions)
+  .then((cli) => cli.db(dbName))
   .catch((err) => {
     console.log(`Error connecting to database: ${err.stack}`);
     process.exit(1);
@@ -18,7 +18,7 @@ async function put(scoresId, dataType, objData) {
   objData.scoresId = scoresId;
   objData.dataType = dataType;
   (await db).collection(collectionName)
-    .replaceOne({ scoresId, dataType }, objData, { upsert: true });
+    .replaceOne({ scoresId, dataType }, objData, { upsert: true })
     .catch((err) => {
       console.log(`Error inserting object: ${err.stack}`);
       throw(err);
