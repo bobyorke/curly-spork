8febc6390ade61b6fa0c066d99e8e0526b8ae1b8
Robert Yorke <robert.yorke@gmail.com>
2020-04-18 15:39:50 +0100

    added missing file

---
+++ src/components/ActiveVoter.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <h2 class="text-center">Incoming scores</h2>
+    <div class="container-fluid">
+      <div class="row active-score" v-for="sc in scores" :key="sc._id">
+        <div class="col-10">
+          {{ sc.country }}, {{ sc.year}}
+        </div>
+        <div class="col-2">
+          {{ sc.activeScore }}
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ActiveVoter',
+  props: {
+    scores: Object,
+  },
+};
+</script>
+
+<style scoped>
+.active-score {
+  font-size: 2.6vh;
+  font-variant: small-caps;
+}
+</style>
